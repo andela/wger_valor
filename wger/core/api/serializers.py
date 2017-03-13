32b0c290069a870d516aeea915cefdee3051e932
andela-ik <ian.kingori@andela.com>
2017-03-13 10:44:09 +0300

    [ft #140647859] Added User Model Serializer

--- wger/core/api/serializers.py
+++ wger/core/api/serializers.py
@@ -16,7 +16,10 @@
 # along with Workout Manager.  If not, see <http://www.gnu.org/licenses/>.
 
 from rest_framework import serializers
+from rest_framework.authtoken.models import Token
 
+
+from django.contrib.auth.models import User
 from wger.core.models import (
     UserProfile,
     Language,
@@ -24,6 +27,22 @@ from wger.core.models import (
     License,
     RepetitionUnit,
     WeightUnit)
+
+
+class UserSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'password')
+        extra_kwargs = {'password': {'write_only': True}}
+
+    def create(self, validated_data):
+        password = validated_data.pop('password', None)
+        user = self.Meta.model(**validated_data)
+        if password is not None:
+            user.set_password(password)
+        user.save()
+        return user
 
 
 class UserprofileSerializer(serializers.ModelSerializer):
